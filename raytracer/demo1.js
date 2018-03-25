90ee483036c36ee1f50f9ef542dc4f47a03fb6ac
tim <tjhickey@brandeis.edu>
2018-03-25 13:28:44 -0400

    version 1 of the raytracer -- without lighting

---
+++ raytracer/demo1.js
@@ -0,0 +1,37 @@
+
+
+function runTest(){
+	canvas.width=900
+	canvas.height=900
+	const renderer = new Renderer(900,900)
+	const scene = new Scene()
+	const camera = new Camera()
+	camera.translateZ(10)
+	const s1 = new Sphere(new Vector3(-2,0,-8),2)
+	const s2 = new Sphere(new Vector3(2,0,-8),2)
+	const p1 = new Plane(new Vector3(0,-20,-100),new Vector3(100,0,0), new Vector3(0,100,0))
+	const p2 = new Plane(new Vector3(50,0,-100),new Vector3(0,0,100), new Vector3(0,100,0))
+
+	scene.addObject(s1)
+	scene.addObject(s2)
+	scene.addObject(p1)
+	scene.addObject(p2)
+	for(let i=0; i<10;i++){
+		const a = randNumInRange(-50,50)
+		const b = randNumInRange(-50,50)
+		const c = randNumInRange(-100,-50)
+		scene.addObject(new Sphere(new Vector3(a,b,c),randNumInRange(1,10)))
+		console.log("adding"+JSON.stringify([a,b,-c]));
+	}
+
+	renderer.render(scene,camera)
+}
+
+function randNumInRange(a,b){
+	// generates a random number in the range [a,b]
+	const d = b-a
+	const r = Math.random()*d + a
+	return r
+}
+
+runTest()
